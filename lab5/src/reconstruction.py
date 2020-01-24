5d1ae37a016e8610456365b195600d9db84a69d9
RogerCasalsVilardell <43968704+RogerCasalsVilardell@users.noreply.github.com>
2020-01-24 12:15:42 +0100

    Estimation first projective camera

--- lab5/src/reconstruction.py
+++ lab5/src/reconstruction.py
@@ -8,6 +8,19 @@ def compute_proj_camera(F, i):
     # Result 9.15 of MVG (v = 0, lambda = 1). It assumes P1 = [I|0]
     # your code here
 
+    # compute epipole
+    e = mth.nullspace(F.T)
+
+    # build [e]_x
+    e_x = np.asarray([[0, -e[2], e[1]],
+           [e[2], 0, -e[0]],
+           [-e[1], e[0], 0]
+    ])
+
+    # compute P
+    P = e_x@F
+    P = np.concatenate((P,e),axis=1)
+
     return P
 
 def estimate_3d_points(P1, P2, xr1, xr2):
